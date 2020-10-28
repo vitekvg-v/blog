2f785de1b118a0cc55e7934c19a6bbda6f0452b3
Victor Grishaev <victor.grishaev@sharp-dev.net>
2020-10-28 09:42:44 +0300

    Add blog filesd

---
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Post } from 'src/app/shared/interfaces';
+import { PostService } from 'src/app/shared/post.service';
+import { AlertService } from '../shared/services/alert.service';
+
+@Component({
+  selector: 'app-edit-page',
+  templateUrl: './edit-page.component.html',
+  styleUrls: ['./edit-page.component.scss']
+})
+export class EditPageComponent implements OnInit, OnDestroy {
+  form: FormGroup;
+  post: Post;
+  submitted = false;
+  uSub: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostService,
+    private alert: AlertService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+      return this.postsService.getById(params['id'])
+    })
+    ).subscribe((post: Post) => {
+      this.post = post;
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required)
+      })
+    })
+  }
+
+  ngOnDestroy(): void{
+    if(this.uSub){
+      this.uSub.unsubscribe();
+    }
+  }
+
+  submit(){
+    if(this.form.invalid){
+      return;
+    }
+
+    this.submitted = true;
+
+    this.uSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title
+    }).subscribe(() => {
+      this.submitted = false;
+      this.alert.warning("Пост был успешно обновлён!")
+    })
+
+  }
+
+}
---
+++ src/app/shared/post.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { fbCreateResponse, Post } from './interfaces';
+import { map } from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+
+export class PostService{
+    constructor(private http: HttpClient){}
+
+    create(post: Post): Observable<Post>{
+        return this.http.post(`${environment.fbDbUrl}/posts.json`, post).pipe(map((response: fbCreateResponse) => {
+                return {
+                    ...post,
+                    id: response.name,
+                    date: new Date(post.date)
+                }
+            }))
+    }
+
+    getAll(){
+        return this.http.get(`${environment.fbDbUrl}/posts.json`)
+            .pipe(map((response: {[key: string]: any}) => {
+                return Object.keys(response).map(key => ({
+                    ...response[key],
+                    id: key,
+                    date: new Date(response[key].date)
+                }))
+            }))
+    }
+
+    getById(id: string): Observable<Post>{
+        return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`)
+            .pipe(map((post: Post) => {
+                return {
+                    ...post,
+                    id,
+                    date: new Date(post.date)
+                }
+            }))
+    }
+
+    remove(id: string): Observable<void>{
+        return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
+    }
+
+    update(post: Post): Observable<Post>{
+        return this.http.patch<Post>(`${environment.fbDbUrl}/posts/${post.id}.json`, post)
+    }
+}
